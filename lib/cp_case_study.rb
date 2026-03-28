789b5d6ea1d03857ed1e6b6a2819385211938250
Oleksandr <noreply@example.com>
2026-03-28 10:12:34 +0200

    Gem init

---
+++ lib/cp_case_study.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "cp_case_study/version"
+
+module CpCaseStudy
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/cp_case_study/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module CpCaseStudy
+  VERSION = "0.1.0"
+end
